479225061fa272bb83050aabf54aafe2355f198f
Alberto Yamil Rios Riquelme <ariosriq@emeal.nttdata.com>
2022-07-19 09:30:42 -0500

    reporte checkstyle

--- ms-product/src/main/java/com/proyecto1/product/ProductApplication.java
+++ ms-product/src/main/java/com/proyecto1/product/ProductApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Microservicio producto.
+ */
 @EnableEurekaClient
 @SpringBootApplication
 public class ProductApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProductApplication.class, args);
+  }
 }
