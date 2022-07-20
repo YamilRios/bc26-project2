fef929ad25aabccc49e2e02c495f3c0847feade9
zoyarce <zoyarce@emeal.nttdata.com>
2022-07-19 23:26:02 -0500

    zoz: fix mockito 3

--- ms-payment/src/test/java/com/proyecto1/payment/PaymentApplicationTests.java
+++ ms-payment/src/test/java/com/proyecto1/payment/PaymentApplicationTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -24,7 +25,7 @@ import java.time.LocalDate;
 
 import static org.mockito.Mockito.times;
 
-
+@SpringBootTest
 class PaymentApplicationTests {
 	@Test
 	void contextLoads() {
