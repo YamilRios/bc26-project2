ee229290d7ad07bdc271e6185e264b359e97d3f8
Yamil <yamilrios96@gmail.com>
2022-07-20 01:48:27 -0500

    Fix eureka server

--- eureka-service/src/main/java/com/project2/eureka/app/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/project2/eureka/app/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.project2.springboot.eureka.app;
+package com.project2.eureka.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringBootEurekaServerApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootEurekaServerApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
